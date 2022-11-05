e0f78f3afafbe849930d93bafeecfb1de22b1a0a
Ingo Ruhnke <grumbel@gmail.com>
2022-11-05 16:50:54 +0100

    Update Nix flake to 22.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -8,29 +8,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+      in {
+        packages = rec {
+          default = txt2png;
+
           txt2png = pkgs.stdenv.mkDerivation {
             pname = "txt2png";
             version = "0.0.0";
+
             src = nixpkgs.lib.cleanSource ./.;
+
             postPatch = ''
               substituteInPlace src/txt2png.cpp \
                 --replace '"VeraMono.ttf"' '"${pkgs.dejavu_fonts}/share/fonts/truetype/DejaVuSansMono.ttf"' \
                 --replace 'VeraMono.ttf' 'DejaVuSansMono.ttf'
             '';
-            enableParallelBuilding = true;
+
             nativeBuildInputs = with pkgs; [
               cmake
               pkg-config
             ];
+
             buildInputs = with pkgs; [
               freetype
               libjpeg
             ];
            };
         };
-        defaultPackage = packages.txt2png;
       }
     );
 }
